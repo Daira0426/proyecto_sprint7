edea3b725bdc30fca2e8409fac2198ad67405248
Daira <daira0426@gmail.com>
2025-03-01 01:01:38 -0500

    Actualiza el archivo README y agrega config.toml

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # proyecto_sprint7
+
+Esta es una aplicación web desarrollada con Streamlit que analiza datos de anuncios de venta de coches. La aplicación proporciona varias visualizaciones interactivas, incluyendo histogramas y gráficos de dispersión, para explorar las características de los datos.
---
+++ notebooks/app_proyecto.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv(
+    r"C:\\Users\\daira\\Downloads\\Python\\proyecto_sprint7\\vehicles_us.csv")
+
+
+st.header('Análisis de datos de anuncios de venta de coches')
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botón para el gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos')
+    fig = px.scatter(car_data, x="odometer", y="price",
+                     title="Relación entre kilometraje y precio")
+    st.plotly_chart(fig, use_container_width=True)
